688062d1d9906775a87a0118089c833b715af302
dingbo6666 <978723801@qq.com>
2017-11-16 14:32:58 +0800

    第十三次mysql数据表补传

---
+++ im_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `im_user`(`id`, `username`, `password`, `create_time`) VALUES (1, 'ss', 'f65fb353a9e2d1544654498b7ee82d42', 1510709894);
+INSERT INTO `im_user`(`id`, `username`, `password`, `create_time`) VALUES (2, 'wangye', 'f65fb353a9e2d1544654498b7ee82d42', 1510813013);
